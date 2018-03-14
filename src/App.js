9969a4ae59cad2cdb3301139edfe1bc9a8c05fe6
Rodrigo Stuchi <rod.stuchi@gmail.com>
2018-03-14 01:38:15 -0300

    css enhancemnts

--- src/App.js
+++ src/App.js
@@ -14,14 +14,55 @@ injectGlobal`
     font-family: 'Roboto', sans-serif;
   }
 
+  body {
+    padding: 40px 0;
+    background: rgba(242,242,242,1);
+    background: -moz-linear-gradient(top, rgba(242,242,242,1) 0%, rgba(242,242,242,1) 50%, rgba(133,133,133,1) 100%);
+    background: -webkit-gradient(left top, left bottom, color-stop(0%, rgba(242,242,242,1)), color-stop(50%, rgba(242,242,242,1)), color-stop(100%, rgba(133,133,133,1)));
+    background: -webkit-linear-gradient(top, rgba(242,242,242,1) 0%, rgba(242,242,242,1) 50%, rgba(133,133,133,1) 100%);
+    background: -o-linear-gradient(top, rgba(242,242,242,1) 0%, rgba(242,242,242,1) 50%, rgba(133,133,133,1) 100%);
+    background: -ms-linear-gradient(top, rgba(242,242,242,1) 0%, rgba(242,242,242,1) 50%, rgba(133,133,133,1) 100%);
+    background: linear-gradient(to bottom, rgba(242,242,242,1) 0%, rgba(242,242,242,1) 50%, rgba(133,133,133,1) 100%);
+    background-attachment: fixed;
+  }
+
+  @media only screen and (max-width: 900px) {
+    body {
+      padding: 0;
+    }
+  }
+
   @media print {
+    body {
+      background: none;
+    }
     .noprint {
       display: none;
     }
+  }
 `;
 
 const Content = styled.div`
-    margin: 2vh 2vw;
+    border-radius: 20px;
+    padding: 12px;
+    box-shadow: 0px 2px 20px 1px rgba(0,0,0,0.75);
+    background-color: rgba(255, 255, 255, 0.8);
+    margin: 20px auto;
+    max-width: 900px;
+
+    @media only screen and (max-width: 900px) {
+      border-radius: 0;
+      margin: 0;
+      background-color: #fffff;
+    }
+
+    @media print {
+      background-color: #fffff;
+      padding: 3px;
+      border-radius: 0;
+      box-shadow: none;
+      max-width: auto;
+    }
 `;
 
 class App extends Component {
